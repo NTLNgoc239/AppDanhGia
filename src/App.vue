24424ee8a7778161c6d5c5efe6168e9f862d9ce9
NTLNgoc <nguyenngochcmus@gmail.com>
2022-01-20 18:09:23 +0700

    test app

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <!-- <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div> -->
+  <b-container fluid id="app">
+    <div class="title">
+      <img src="../src/assets/logo.svg" />
+    </div>
+    <!-- <home></home> -->
+    <router-view />
+  </b-container>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+.container-fluid {
+  background-color: white;
+  height: 80px;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  border-bottom: 1px solid rgb(184, 179, 179);
+  margin: 0;
+  margin-bottom: 20px;
+}
+.container-fluid img {
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+body {
+  margin: 0;
+}
+
+/* tablet */
+@media (min-width: 768px) {
+  .container-fluid img {
+    height: 10em;
+  }
+  .title img {
+    height: auto;
+  }
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) {
+  .container-fluid img {
+    height: 12em;
+  }
+  .title img {
+    height: auto;
+  }
+}
+
+/*Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+  .container-fluid img {
+    height: 12em;
+  }
+  .title img {
+    height: auto;
+  }
+}
+</style>
+
+//
+<script>
+//     import Home from './views/Home.vue';
+
+//     export default {
+//         components: {
+//             home: Home
+//         }
+//     }
+//
+</script>
---
+++ src/components/InfoCustomer.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="info-customer">
+    <div class="image">
+      <img src="../assets/istockphoto-922962354-612x612.jpg" />
+    </div>
+    <div class="name">
+      <p>{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info-customer {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+img {
+  border: 1px solid #2787f4;
+
+  border-radius: 0;
+  width: 200px;
+  height: 250px;
+}
+.image {
+  padding: 20px;
+}
+.name {
+  font-size: 28px;
+  font-weight: bold;
+  color: #292c2e;
+}
+@media (min-width: 992px) {
+  img {
+    border-radius: 0;
+    width: 200px;
+    height: 250px;
+  }
+}
+/* Small devices (landscape phones, 576px and up) */
+@media (min-width: 768px) {
+  img {
+    border-radius: 50%;
+    width: 200px;
+    height: 250px;
+  }
+}
+
+@media (min-width: 1200px) {
+  img {
+    border-radius: 0;
+    width: 200px;
+    height: 250px;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "InfoCustomer",
+  props: {
+    name: String,
+  },
+};
+</script>
